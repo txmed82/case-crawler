5a3502694179310338da98ed9d3d99581c39d29c
Colin Son <colinson@fastmail.com>
2026-05-08 04:35:12 -0500

    Add medication reconciliation export (#207)

--- ui/src/api/client.ts
+++ ui/src/api/client.ts
@@ -255,6 +255,7 @@ export type ExportFormat =
   | "raw_jsonl"
   | "sft_jsonl"
   | "note_fact_sft_jsonl"
+  | "medication_reconciliation_jsonl"
   | "chat_jsonl"
   | "tool_call_jsonl"
   | "multimodal_jsonl"
